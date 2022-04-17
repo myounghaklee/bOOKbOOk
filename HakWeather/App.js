c5981a9083bfde5530c225d6fba7f2abb4937d09
myounghaklee <leemh9207@ajou.ac.kr>
2022-04-17 20:07:39 +0900

    오타수정

--- HakWeather/App.js
+++ HakWeather/App.js
@@ -1,12 +1,12 @@
 import React from "react";
-import { View} from "react-native";
+import { View } from "react-native";
 
 export default function App(){
   return (
     <View>
-      <View styel ={{width:100, height:100, backgroundColor : "tomato"}}></View>
-      <View styel ={{width:100, height:100, backgroundColor: "teal"}}></View>
-      <View styel ={{width:100, height:100, backgroundColor : "orange"}}></View>
+      <View style ={{width:100, height:100, backgroundColor : "tomato"}}></View>
+      <View style ={{width:100, height:100, backgroundColor: "skyblue"}}></View>
+      <View style ={{width:100, height:100, backgroundColor : "orange"}}></View>
     </View>
-  )
+  );
 }
