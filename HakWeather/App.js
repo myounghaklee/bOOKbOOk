a76b600343eecbe489aea7c9e42cda06321e2aa8
myounghaklee <leemh9207@ajou.ac.kr>
2022-04-18 23:06:37 +0900

    temp,description add

--- HakWeather/App.js
+++ HakWeather/App.js
@@ -6,13 +6,19 @@ export default function App() {
   return (
     <View style={style.container}>
       <View style={style.city}>
-        <Text>Seoul</Text>
+        <Text style={style.cityName}>Seoul</Text>
       </View>
-      <View style={style.weather}></View>
+      <View style={style.weather}>
+        <View style={style.day}>
+          <Text style={style.temp}>27</Text>
+          <Text style={style.description}>Sunny</Text>
+        </View>
+        </View>
         
     </View>
   );
 }
+
 
 const style = StyleSheet.create({
   container:{
@@ -20,10 +26,28 @@ const style = StyleSheet.create({
   },
   city:{
     flex : 1, 
-    backgroundColor :"lemonchiffon"
+    backgroundColor :"lemonchiffon",
+    justifyContent:"center",
+    alignItems:"center",
+  },
+  cityName:{
+    fontSize:68,
+    fontWeight:"500",
   },
   weather:{
     flex: 3, 
-    backgroundColor:"lightskyblue"
+    backgroundColor:"lightskyblue",
+  },
+  day:{
+    flex :1,
+    alignItems : "center",
+  },
+  temp:{
+    marginTop: 50,
+    fontSize:178,
+  },
+  description:{
+    marginTop:-30,
+    fontSize:60,
   },
 });
