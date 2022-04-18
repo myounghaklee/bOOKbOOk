06ab4a96fc558dd5c91dd8f9551f0929015653aa
myounghaklee <leemh9207@ajou.ac.kr>
2022-04-18 18:12:00 +0900

    stylesheet 추가

--- HakWeather/App.js
+++ HakWeather/App.js
@@ -1,15 +1,28 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { View , StyleSheet} from "react-native";
+import { View ,Text, StyleSheet} from "react-native";
 
 export default function App() {
   return (
     <View style={style.container}>
+      <View>
+        <Text>Seoul</Text>
+      </View>
+
     </View>
   );
 }
+
 const style = StyleSheet.create({
   container:{
     flex:1, backgroundColor:"aliceblue"
-  }
-})
+  },
+  city:{
+    flex : 1, 
+    backgroundColor :"lemonchiffon"
+  },
+  weather:{
+    flex: 1, 
+    backgroundColor:"lightskyblue"
+  },
+});
